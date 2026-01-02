fb41daec513814e9812723b96ce322a896510916
Javascriptbabe <javascriptbabe@gmail.com>
2026-01-02 13:38:05 +0100

    done

--- app/layout.js
+++ app/layout.js
@@ -25,6 +25,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={`${catamaran.variable} ${poppins.variable} antialiased`}>
         <Navbar />
         {children}
